02ab2575bb074fefcbaa3f35babbfcaa2654670f
sachin17115285 <45783606+sachin17115285@users.noreply.github.com>
2021-06-28 20:14:36 +0530

    add  allowed host

--- learning_users/learning_users/settings.py
+++ learning_users/learning_users/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-g1vajd=@)s#g3$#g-m0$*m-4nxpmasfp%tp4%k#*x%z^27r8#!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['http://sachin357.pythonanywhere.com/',]
 
 
 # Application definition
